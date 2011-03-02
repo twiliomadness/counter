c6a64de30dbe330c16ad3f4656ff98ae9bc1c201
Devin Walters and Greg Tracy and Bradley Grzesiak and Brian Beermann and Steve Faulkner and Philip Crawford <dev+defn+gtracy+listrophy+powdernine+southpolesteve+wiscoDude@bendyworks.com>
2011-03-01 20:18:02 -0600

    Add events and things

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,18 @@
 class EventsController < ApplicationController
 
   def create
+    if Event.create_from_twilio(params)
+      head 200
+    else
+      raise 'meh!'
+    end
+  end
+
+  def validate_twilio
+    # util = Twilio::Utils.new(ENV['TWILIO_ID'], ENV['TWILIO_TOKEN'])
+    # if util.validateRequest(params)
+    # else
+    # end
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  # belongs_to :thing
+  belongs_to :thing
+
+  def self.create_from_twilio params
+    body = params[:Body]
+
+    event = Event.new
+    split_body = body.split(' ')
+    event.number = split_body.first
+    event.thing = Thing.find_or_initialize_by_name(split_body[1..-1].join(' '))
+    event.user = User.find_or_initialize_by_phone_number(params[:From])
+    event.save!
+  end
+
 end
