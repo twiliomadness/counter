2c7b8d65c2952114bc658a9bb4634c65f6a302c9
Devin Walters and Greg Tracy and Bradley Grzesiak and Brian Beermann and Steve Faulkner and Philip Crawford <dev+defn+gtracy+listrophy+powdernine+southpolesteve+wiscoDude@bendyworks.com>
2011-03-01 22:00:53 -0600

    Add `list` capability

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,51 @@
 class EventsController < ApplicationController
+  before_filter :find_user
 
   def create
+    if params[:Body].match(/list/i)
+      list_from_twilio
+    else
+      create_from_twilio
+    end
+  end
+
+private
+
+  def list_from_twilio
+    events = @user.events
+    ret = {}
+    res.each_pair{|k,v| ret[Thing.find(k).name] = v.map(&:number).sum}
+    text = ret.sort.map{|x| x.join(': ')}.join(', ')
+
+    send_text params[:From], text
+  end
+
+  def create_from_twilio
     if event = Event.create_from_twilio(params)
 
-      twilio = Twilio::RestAccount.new(ENV['TWILIO_ID'], ENV['TWILIO_TOKEN'])
-
-      user = User.find_by_phone_number(params[:From])
-      d = {
-        'From' => '6082475032',
-        'To' => params[:From],
-        'Body' => "Total: #{user.events.where(:thing_id => event.thing).sum(:number)}"
-      }
-
-      path = "/2010-04-01/Accounts/#{ENV['TWILIO_ID']}/SMS/Messages"
-      Rails.logger.warn "PATH: #{path}"
-      resp = twilio.request(path, 'POST', d)
-
-      resp.error! unless resp.kind_of? Net::HTTPSuccess
-      puts "code: %s\nbody: %s" % [resp.code, resp.body]
+      text = "Total: #{@user.events.where(:thing_id => event.thing).sum(:number)}"
+      send_text params[:From], text
       head 200
     else
       raise 'meh!'
     end
+  end
+
+  def send_text to, content
+      twilio = Twilio::RestAccount.new(ENV['TWILIO_ID'], ENV['TWILIO_TOKEN'])
+
+      d = {
+        'From' => '6082475032',
+        'To' => to,
+        'Body' => content
+      }
+
+      path = "/2010-04-01/Accounts/#{ENV['TWILIO_ID']}/SMS/Messages"
+      resp = twilio.request(path, 'POST', d)
+
+      resp.error! unless resp.kind_of? Net::HTTPSuccess
+      puts "code: %s\nbody: %s" % [resp.code, resp.body]
+
   end
 
   def validate_twilio
@@ -29,6 +53,10 @@ class EventsController < ApplicationController
     # if util.validateRequest(params)
     # else
     # end
+  end
+
+  def find_user
+    @user = User.find_by_phone_number(params[:From])
   end
 
 end
