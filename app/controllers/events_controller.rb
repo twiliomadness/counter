b31adbd8b9af2a6380acd2aa7e6263d84a61b241
Devin Walters and Greg Tracy and Bradley Grzesiak and Brian Beermann and Steve Faulkner and Philip Crawford <dev+defn+gtracy+listrophy+powdernine+southpolesteve+wiscoDude@bendyworks.com>
2011-03-01 21:05:58 -0600

    Respond!

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,22 @@
 class EventsController < ApplicationController
 
   def create
-    if Event.create_from_twilio(params)
+    if event = Event.create_from_twilio(params)
+
+      twilio = Twilio::RestAccount.new(ENV['TWILIO_ID'], ENV['TWILIO_TOKEN'])
+
+      user = User.find_by_phone_number(params[:From])
+      d = {
+        'From' => '6082475032',
+        'To' => params[:From],
+        'Body' => "Total: #{user.events.where(:thing => event.thing).sum(:number)}"
+      }
+
+      resp = account.request("/2010-04-01/Accounts/#{ENV['TWILIO_ID']}/Sms",
+                             'POST', d)
+
+      resp.error! unless resp.kind_of? Net::HTTPSuccess
+      puts "code: %s\nbody: %s" % [resp.code, resp.body]
       head 200
     else
       raise 'meh!'
